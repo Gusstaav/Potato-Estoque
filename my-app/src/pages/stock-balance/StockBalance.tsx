1e43fe2767739de500e4cef3f5882b2ab2d299ea
Gusstaav <83920275+Gusstaav@users.noreply.github.com>
2026-09-06 19:06:14 +0000

    tela-saldo-finalizada

--- my-app/src/pages/stock-balance/StockBalance.tsx
+++ my-app/src/pages/stock-balance/StockBalance.tsx
@@ -4,29 +4,68 @@ import './StockBalance.css'
 
 function StockBalance(){
     return(
-        <section id='section-home'>
-            <div id="container-home-dashboard">
-                <NavLink id='link-home' to="/conference">
-                    <div id="box-info-home">
-                        <p id='title-conference'>Pendente de conferencia</p>
-                        <p id='total-itens-pendente-conference'>750</p>
-                    </div>
-                </NavLink>     
-                
-                <NavLink id='link-home' to="/guarda">
-                    <div id="box-info-home">
-                        <p id='title-guarda'>Pendente de guarda</p>
-                        <p id='total-itens-pendente-guarda'>750</p>
-                    </div>
-                </NavLink>
-                
-                <NavLink id='link-home' to="/picking">
-                    <div id="box-info-home">
-                        <p id='title-picking'>Pendente de separação</p>
-                        <p id='total-itens-pendente-picking'>750</p>
-                    </div>
-                </NavLink>
+        <section id="Section-StockBalance">
+            <div id="Container-StockBalance-search">
+                <p id="Container-StockBalance-search-title">Consultar item</p>
+               <input type='text' id='StockBalance-search-input' placeholder='Codigo' />
+            </div>
 
+            <div id='List-StockBalance-Container'>
+               
+                
+                <div id="StockBalance-list-header">
+                    <p id='StockBalance-list-header-PartNumber'>Código: 123</p>
+                    <p id='StockBalance-list-header-Description'>Pneu de carro Palio 2013</p>
+                </div>
+                
+                <div id='StockBalance-status'>
+                    <div id='StockBalance-status-box-conference'>
+                        <p id='StockBalance-status-box-conference-title'>Total em conferencia</p>
+                        <p id='StockBalance-status-box-conference-info'>50</p>
+                    </div>
+
+                    <div id='StockBalance-status-box-stock'>
+                        <p id='StockBalance-status-box-stock-title'>Total em estoque</p>
+                        <p id='StockBalance-status-box-stock-info'>10</p>
+                    </div>
+                </div>
+
+                <div id='container-info-StockBalance-Header'>
+                    <p>Deposito</p>
+                    <p>Localização</p>
+                    <p>Quantidade</p>
+                </div>
+
+                <div id='container-info-StockBalance-Itens'>
+                    <p id='info-StockBalance-Itens'>2000</p>
+                    <p id='info-StockBalance-Itens'>R01.A5</p>
+                    <p id='info-StockBalance-Itens'>10</p>
+
+                    <p id='info-StockBalance-Itens'>2000</p>
+                    <p id='info-StockBalance-Itens'>R01.A5</p>
+                    <p id='info-StockBalance-Itens'>10</p>
+
+                    <p id='info-StockBalance-Itens'>2000</p>
+                    <p id='info-StockBalance-Itens'>R01.A5</p>
+                    <p id='info-StockBalance-Itens'>10</p>
+
+                    <p id='info-StockBalance-Itens'>2000</p>
+                    <p id='info-StockBalance-Itens'>R01.A5</p>
+                    <p id='info-StockBalance-Itens'>10</p>
+
+                    <p id='info-StockBalance-Itens'>2000</p>
+                    <p id='info-StockBalance-Itens'>R01.A5</p>
+                    <p id='info-StockBalance-Itens'>10</p>
+
+                    <p id='info-StockBalance-Itens'>2000</p>
+                    <p id='info-StockBalance-Itens'>R01.A5</p>
+                    <p id='info-StockBalance-Itens'>10</p>
+                </div>
+            </div>
+
+            <div id='container-info-total'>
+            <p id='total-itens-StockBalance'>Total disponivel no estoque:</p>
+            <p id='total-itens-StockBalance-info'>2000</p>
             </div>
         </section>
     
